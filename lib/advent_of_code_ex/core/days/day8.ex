0a37fe8f01e3722db6c662f6fc7417348a90219a
corsen-olbin <corbinolsen97@gmail.com>
2025-12-09 18:08:43 -0600

    day 8

--- lib/advent_of_code_ex/core/days/day8.ex
+++ lib/advent_of_code_ex/core/days/day8.ex
@@ -14,8 +14,18 @@ defmodule AdventOfCodeEx.Core.Days.Day8 do
     |> Enum.reduce(1, fn x, acc -> length(x) * acc end)
   end
 
-  def part_2(_input) do
-    :unimplemented
+  def part_2(input) do
+    {{x, _, _}, {x2, _, _}} =
+      input
+      |> read_input
+      |> find_all_euc_distance
+      #|> IO.inspect()
+      |> find_num_shortest_pairs(10000000000) # 1000 for real one, 10 for example
+      #|> IO.inspect(limit: :infinity)
+      |> build_circuits2([], nil)
+      #|> IO.inspect(limit: :infinity, width: 100)
+
+    x * x2
   end
 
   def read_input(input) do
@@ -55,11 +65,34 @@ defmodule AdventOfCodeEx.Core.Days.Day8 do
       p1i === nil and is_number(p2i) -> build_circuits(list, List.update_at(acc, p2i, fn l -> [p1 | l] end))
       p2i === nil and is_number(p1i) -> build_circuits(list, List.update_at(acc, p1i, fn l -> [p2 | l] end))
       p2i !== p1i and is_number(p2i) and is_number(p1i) ->
+        # combine two lists
         {p2l, a} = List.pop_at(acc, p2i)
         new_p1i = Enum.find_index(a, fn l -> p1 in l end)
         new_acc = List.update_at(a, new_p1i, fn l -> l ++ p2l end)
         build_circuits(list, new_acc)
       p2i === nil and p1i === nil -> build_circuits(list, [[p1, p2] | acc])
+    end
+  end
+
+  def build_circuits2([], _acc, final_binder), do: final_binder
+  def build_circuits2([{{p1, p2}, _} | list], acc, final_binder) do
+    p1i = Enum.find_index(acc, fn l -> p1 in l end)
+    p2i = Enum.find_index(acc, fn l -> p2 in l end)
+
+    cond do
+      p1i === p2i and is_number(p1i) -> build_circuits2(list, acc, final_binder)
+      p1i === nil and is_number(p2i) -> build_circuits2(list, List.update_at(acc, p2i, fn l -> [p1 | l] end), (if length(acc) == 1, do: {p1,p2}, else: final_binder))
+      p2i === nil and is_number(p1i) -> build_circuits2(list, List.update_at(acc, p1i, fn l -> [p2 | l] end), (if length(acc) == 1, do: {p1,p2}, else: final_binder))
+      p2i !== p1i and is_number(p2i) and is_number(p1i) ->
+        # combine two lists
+        {p2l, a} = List.pop_at(acc, p2i)
+        new_p1i = Enum.find_index(a, fn l -> p1 in l end)
+        new_acc = List.update_at(a, new_p1i, fn l -> l ++ p2l end)
+        case length(new_acc) do
+          1 -> build_circuits2(list, new_acc, {p1, p2})
+          _ -> build_circuits2(list, new_acc, final_binder)
+        end
+      p2i === nil and p1i === nil -> build_circuits2(list, [[p1, p2] | acc], final_binder)
     end
   end
 end
