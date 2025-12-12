4c329c07976c287b46a76be47c4525f9064989a4
corsen-olbin <corbinolsen97@gmail.com>
2025-12-11 18:21:04 -0600

    day 11 part 1

--- lib/advent_of_code_ex/core/days/day11.ex
+++ lib/advent_of_code_ex/core/days/day11.ex
@@ -1,11 +1,39 @@
 defmodule AdventOfCodeEx.Core.Days.Day11 do
 
 
-  def part_1(_input) do
-    :unimplemented
+  def part_1(input) do
+    input
+    |> read_input
+    |> traverse_rec("you")
+    |> elem(1)
   end
 
   def part_2(_input) do
     :unimplemented
   end
+
+  def read_input(input) do
+    input
+    |> String.split("\r\n")
+    |> Enum.map(fn l -> String.split(l, ": ") end)
+    |> Enum.reduce(%{}, fn [start, outputs], acc -> Map.put_new(acc, start, {String.split(outputs, " "), nil}) end)
+  end
+
+  def traverse_rec(map, "out"), do: {map, 1}
+  def traverse_rec(map, node) do
+    {outs, num } = Map.get(map, node)
+
+    if num !== nil do
+      {map, num}
+    else
+      {final_map, total} =
+        outs
+        |> Enum.reduce({map, 0}, fn o, {m, n} ->
+          {new_map, num} = traverse_rec(m, o)
+          {new_map, num + n}
+        end)
+      { Map.put(final_map, node, {outs, total}), total }
+    end
+  end
+
 end
