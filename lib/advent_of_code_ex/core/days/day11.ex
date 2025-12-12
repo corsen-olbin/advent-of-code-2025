55a131de634f804a85d2d4e605dfaf33fd49b191
corsen-olbin <corbinolsen97@gmail.com>
2025-12-11 20:20:10 -0600

    d11p2

--- lib/advent_of_code_ex/core/days/day11.ex
+++ lib/advent_of_code_ex/core/days/day11.ex
@@ -8,15 +8,18 @@ defmodule AdventOfCodeEx.Core.Days.Day11 do
     |> elem(1)
   end
 
-  def part_2(_input) do
-    :unimplemented
+  def part_2(input) do
+    input
+    |> read_input()
+    |> traverse2_rec(false, false, "svr")
+    |> elem(1)
   end
 
-  def read_input(input) do
+  def read_input(input, default \\ nil) do
     input
     |> String.split("\r\n")
     |> Enum.map(fn l -> String.split(l, ": ") end)
-    |> Enum.reduce(%{}, fn [start, outputs], acc -> Map.put_new(acc, start, {String.split(outputs, " "), nil}) end)
+    |> Enum.reduce(%{}, fn [start, outputs], acc -> Map.put_new(acc, start, {String.split(outputs, " "), default}) end)
   end
 
   def traverse_rec(map, "out"), do: {map, 1}
@@ -36,4 +39,22 @@ defmodule AdventOfCodeEx.Core.Days.Day11 do
     end
   end
 
+  def traverse2_rec(map, dac, fft, "out"), do: {map, (if dac and fft, do: 1, else: 0)}
+  def traverse2_rec(map, dac, fft, node) do
+    {outs, _num } = Map.get(map, node)
+    new_dac = (node === "dac") or dac
+    new_fft = (node === "fft") or fft
+
+    # if num !== nil and dac and fft do
+    #   {map, num}
+    # else
+      {final_map, total} =
+        outs
+        |> Enum.reduce({map, 0}, fn o, {m, n} ->
+          {new_map, num} = traverse2_rec(m, new_dac, new_fft, o)
+          {new_map, num + n}
+        end)
+      { Map.put(final_map, node, {outs, total}), total }
+    #end
+  end
 end
