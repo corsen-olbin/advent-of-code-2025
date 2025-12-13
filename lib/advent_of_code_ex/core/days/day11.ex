b66889df95c5064fd0b30ee5ff849c5550a3f4dc
corsen-olbin <corbinolsen97@gmail.com>
2025-12-12 22:37:56 -0600

    day 11 part 2 fr

--- lib/advent_of_code_ex/core/days/day11.ex
+++ lib/advent_of_code_ex/core/days/day11.ex
@@ -4,15 +4,14 @@ defmodule AdventOfCodeEx.Core.Days.Day11 do
   def part_1(input) do
     input
     |> read_input
-    |> traverse_rec("you")
+    |> traverse_rec("you", "out")
     |> elem(1)
   end
 
   def part_2(input) do
     input
     |> read_input()
-    |> traverse2_rec(false, false, "svr")
-    |> elem(1)
+    |> traverse2()
   end
 
   def read_input(input, default \\ nil) do
@@ -22,8 +21,16 @@ defmodule AdventOfCodeEx.Core.Days.Day11 do
     |> Enum.reduce(%{}, fn [start, outputs], acc -> Map.put_new(acc, start, {String.split(outputs, " "), default}) end)
   end
 
-  def traverse_rec(map, "out"), do: {map, 1}
-  def traverse_rec(map, node) do
+  def traverse2(map) do
+    {_, count1 } = traverse_rec(map, "svr", "fft")
+    {_, count2 } = traverse_rec(map, "fft", "dac")
+    {_, count3 } = traverse_rec(map, "dac", "out")
+    count1 * count2 * count3
+  end
+
+  def traverse_rec(map, final, final), do: {map, 1}
+  def traverse_rec(map, "out", _), do: {map, 0}
+  def traverse_rec(map, node, final) do
     {outs, num } = Map.get(map, node)
 
     if num !== nil do
@@ -32,29 +39,10 @@ defmodule AdventOfCodeEx.Core.Days.Day11 do
       {final_map, total} =
         outs
         |> Enum.reduce({map, 0}, fn o, {m, n} ->
-          {new_map, num} = traverse_rec(m, o)
+          {new_map, num} = traverse_rec(m, o, final)
           {new_map, num + n}
         end)
       { Map.put(final_map, node, {outs, total}), total }
     end
-  end
-
-  def traverse2_rec(map, dac, fft, "out"), do: {map, (if dac and fft, do: 1, else: 0)}
-  def traverse2_rec(map, dac, fft, node) do
-    {outs, _num } = Map.get(map, node)
-    new_dac = (node === "dac") or dac
-    new_fft = (node === "fft") or fft
-
-    # if num !== nil and dac and fft do
-    #   {map, num}
-    # else
-      {final_map, total} =
-        outs
-        |> Enum.reduce({map, 0}, fn o, {m, n} ->
-          {new_map, num} = traverse2_rec(m, new_dac, new_fft, o)
-          {new_map, num + n}
-        end)
-      { Map.put(final_map, node, {outs, total}), total }
-    #end
   end
 end
